6fd29e65fa9218dc4c21b165fce26ec29d3893d0
Akrez <akrez.like@gmail.com>
2024-07-03 00:46:41 +0330

    define TelegramApiService and telegramapi config

---
+++ app/Services/TelegramApiService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class TelegramApiService
+{
+    private static function getUrl($path)
+    {
+        return implode('/', [
+            config('telegramapi.base_url'),
+            config('telegramapi.token'),
+            $path,
+        ]);
+    }
+
+    private static function sendPostForm($path, $data = [], $headers = [])
+    {
+        $url = static::getUrl($path);
+
+        return Http::withHeaders($headers)
+            ->asForm()
+            ->post($url, $data)
+            ->json();
+    }
+
+    public static function getMe()
+    {
+        return static::sendPostForm('getMe');
+    }
+
+    public static function setMyCommands($commands, $optionalParameters = [])
+    {
+        $requiredParameters = [
+            'commands' => [],
+        ];
+
+        foreach ($commands as $command => $description) {
+            $requiredParameters['commands'][] = [
+                'command' => $command,
+                'description' => $description,
+            ];
+        }
+        $requiredParameters['commands'] = json_encode($requiredParameters['commands']);
+
+        return static::sendPostForm('setMyCommands', array_merge_recursive_distinct(
+            $optionalParameters,
+            $requiredParameters
+        ));
+    }
+
+    public static function getUpdates($offset = null, $limit = 200)
+    {
+        return static::sendPostForm('getUpdates', [
+            'limit' => $limit,
+            'offset' => $offset,
+        ]);
+    }
+
+    public static function sendMessage($chatId, $text, $optionalParameters = [])
+    {
+        $requiredParameters = [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ];
+
+        return static::sendPostForm('sendMessage', array_merge_recursive_distinct(
+            $optionalParameters,
+            $requiredParameters
+        ));
+    }
+}
---
+++ config/telegramapi.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'base_url' => env('TELEGRAM_BASE_URL', 'https://api.telegram.org'),
+    'token' => env('TELEGRAM_TOKEN'),
+];
