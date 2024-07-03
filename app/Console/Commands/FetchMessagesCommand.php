362bed5b7dab43ef998dd617cce3715fdc65b8fa
Akrez <akrez.like@gmail.com>
2024-07-04 03:05:43 +0330

    FetchMessagesCommand

--- app/Console/Commands/FetchMessagesCommand.php
+++ app/Console/Commands/FetchMessagesCommand.php
@@ -2,10 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Message;
-use App\Services\TelegramApiService;
+use App\Services\TelegramService;
 use Illuminate\Console\Command;
-use Illuminate\Support\Arr;
 
 class FetchMessagesCommand extends Command
 {
@@ -28,29 +26,6 @@ class FetchMessagesCommand extends Command
      */
     public function handle()
     {
-        $maxId = Message::max('id') + 0;
-        $response = TelegramApiService::getUpdates($maxId + 1);
-        $results = Arr::get($response, 'result', []);
-
-        foreach ($results as $result) {
-
-            $id = $result['update_id'];
-            $content = ($result['message'] ?? $result['edited_message']);
-            $messageText = ($content['text'] ?? null);
-
-            $message = Message::create([
-                'id' => $id,
-                'chat_id' => $id,
-                'content' => json_encode($content),
-                'message_text' => $messageText,
-            ]);
-
-            if (
-                isset($content['from']['is_bot']) and
-                $content['from']['is_bot']
-            ) {
-                $message->delete();
-            }
-        }
+        TelegramService::fetchMessages();
     }
 }
