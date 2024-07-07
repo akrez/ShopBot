474aa4d115572adc85f556444c6ad98455fdb9c0
Akrez <akrez.like@gmail.com>
2024-07-08 01:14:03 +0330

    fix br

--- app/Supports/MessageProcessors/SearchMessageProcessor.php
+++ app/Supports/MessageProcessors/SearchMessageProcessor.php
@@ -8,11 +8,11 @@ class SearchMessageProcessor extends MessageProcessor
 {
     public function process()
     {
-        return (new TelegramApiService($this->bot))->sendMessage(
+        (new TelegramApiService($this->bot))->sendMessage(
             $this->message->chat_id,
-            '<br>جستجو برای</br> '.$this->message->message_text,
+            '***جستجو برای*** '.$this->message->message_text,
             $this->getDefaultReplyMarkup() + [
-                'parse_mode' => 'HTML',
+                'parse_mode' => 'MarkdownV2',
             ]
         );
     }
