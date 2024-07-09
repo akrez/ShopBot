4a18e50db8dd4107f7eef47af057e2332dedc58a
akrez <akrez.like@gmail.com>
2024-07-09 13:06:38 +0330

    SearchMessageProcessor

--- app/Supports/MessageProcessors/SearchMessageProcessor.php
+++ app/Supports/MessageProcessors/SearchMessageProcessor.php
@@ -2,18 +2,44 @@
 
 namespace App\Supports\MessageProcessors;
 
+use App\Services\ShopApi;
 use App\Services\TelegramApiService;
+use App\Traits\MessageProcessorTrait;
+use Illuminate\Support\Str;
 
 class SearchMessageProcessor extends MessageProcessor
 {
+    use MessageProcessorTrait;
+
     public function process()
     {
-        (new TelegramApiService($this->bot))->sendMessage(
-            $this->message->chat_id,
-            'جستجو برای ***'.$this->message->message_text.'***',
-            $this->getDefaultReplyMarkup() + [
-                'parse_mode' => 'MarkdownV2',
-            ]
-        );
+        $productTitleFilter = Str::trim($this->message->message_text);
+        if (! $productTitleFilter) {
+            (new TelegramApiService($this->bot))->sendMessage(
+                $this->message->chat_id,
+                'لطفا قسمتی از عنوان محصول مورد نظر خود را وارد کنید',
+                $this->getDefaultReplyMarkup() + [
+                    'parse_mode' => 'MarkdownV2',
+                ]
+            );
+        }
+
+        $jsonResponse = resolve(ShopApi::class)->json();
+
+        $filterProductIds = collect($jsonResponse)->filter(function ($item) use ($productTitleFilter) {
+            return Str::contains($item['title'], $productTitleFilter, true);
+        })->pluck('id')->toArray();
+
+        if ($filterProductIds) {
+            $this->filterProcess($jsonResponse, $filterProductIds);
+        } else {
+            (new TelegramApiService($this->bot))->sendMessage(
+                $this->message->chat_id,
+                'محصول با عنوانی که شامل ***'.$productTitleFilter.'*** باشد یافت نشد',
+                $this->getDefaultReplyMarkup() + [
+                    'parse_mode' => 'MarkdownV2',
+                ]
+            );
+        }
     }
 }
