06ca2da6255c424ce442b79adc2de88efdbc9d5b
Akrez <akrez.like@gmail.com>
2024-07-12 19:01:33 +0330

    dashboard to app

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -55,12 +55,8 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
-        <header class="navbar-expand-md">
-            <div class="collapse navbar-collapse" id="navbar-menu">
-                <div class="navbar">
-                    <div class="container-xl">
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link" href="/">
