805594bfdd7d9e51596210e8dfa5f146728b53ed
Akrez <akrez.like@gmail.com>
2024-07-13 00:59:41 +0330

    app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ url('images/logo.svg') }}">
     <!-- CSS files -->
-    <link href="{{ url('/tabler/css/tabler.rtl.min.css') }}" rel="stylesheet" />
-    <link href="{{ url('/tabler/css/tabler-flags.rtl.min.css') }}" rel="stylesheet" />
-    <link href="{{ url('/tabler/css/tabler-payments.rtl.min.css') }}" rel="stylesheet" />
-    <link href="{{ url('/tabler/css/tabler-vendors.rtl.min.css') }}" rel="stylesheet" />
-    <link href="{{ url('/vazirmatn/Vazirmatn-font-face.css') }}" rel="stylesheet" />
+    <link href="{{ url('tabler/css/tabler.rtl.min.css') }}" rel="stylesheet" />
+    <link href="{{ url('tabler/css/tabler-flags.rtl.min.css') }}" rel="stylesheet" />
+    <link href="{{ url('tabler/css/tabler-payments.rtl.min.css') }}" rel="stylesheet" />
+    <link href="{{ url('tabler/css/tabler-vendors.rtl.min.css') }}" rel="stylesheet" />
+    <link href="{{ url('vazirmatn/Vazirmatn-font-face.css') }}" rel="stylesheet" />
+    <link href="{{ url('fontawesome/css/all.min.css') }}" rel="stylesheet" />
     <style>
         :root {
             --tblr-font-sans-serif: 'Vazirmatn';
@@ -42,43 +43,67 @@
                     <a href="{{ route('home') }}">{{ config('app.name') }}</a>
                 </h1>
                 <div class="navbar-nav flex-row order-md-last">
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
-                            aria-label="Open user menu">
-                            <span class="avatar avatar-sm"
-                                style="background-image: url({{ url('images/logo.svg') }})"></span>
-                            <div class="d-none d-xl-block ps-2">
-                                <div>AliAkbar Rezaei</div>
-                                <div class="mt-1 small text-secondary">ShahabTahrirBot</div>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                          document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ @route('login') }}">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <i class="fa fa-sign-in" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="nav-link-title">
+                                        {{ __('Login') }}
+                                    </span>
+                                </a>
+                            </li>
+                        @endif
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ @route('register') }}">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <i class="fa fa-user-plus" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="nav-link-title">
+                                        {{ __('Register') }}
+                                    </span>
+                                </a>
+                            </li>
+                        @endif
+                    @else
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
+                                aria-label="Open user menu">
+                                <span class="avatar avatar-sm"
+                                    style="background-image: url({{ url('images/logo.svg') }})"></span>
+                                <div class="d-none d-xl-block ps-2">
+                                    <div>{{ Auth::user()->name }}</div>
+                                    <div class="mt-1 small text-secondary">{{ Auth::user()->name }}</div>
+                                </div>
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+                            <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <i class="fa fa-sign-out" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="nav-link-title">
+                                        {{ __('Logout') }}
+                                    </span>
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                    @endguest
                 </div>
                 <div class="collapse navbar-collapse" id="navbar-menu">
                     <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="/">
-                                    <span
-                                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                                        </svg>
+                                <a class="nav-link" href="{{ @route('home') }}">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <i class="fa fa-home" aria-hidden="true"></i>
                                     </span>
                                     <span class="nav-link-title">
                                         Home
@@ -96,9 +121,16 @@
                 <div class="container-xl">
                     <div class="row g-2 align-items-center">
                         <div class="col">
-                            <h2 class="page-title">
-                                @yield('h2')
-                            </h2>
+                            @hasSection('pretitle')
+                                <div class="page-pretitle">
+                                    @yield('pretitle')
+                                </div>
+                            @endif
+                            @hasSection('title')
+                                <div class="page-title">
+                                    @yield('title')
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -113,7 +145,7 @@
     </div>
     <!-- Libs JS -->
     <!-- Tabler Core -->
-    <script src="{{ url('/tabler/js/tabler.min.js') }}" defer></script>
+    <script src="{{ url('tabler/js/tabler.min.js') }}" defer></script>
 </body>
 
 </html>
