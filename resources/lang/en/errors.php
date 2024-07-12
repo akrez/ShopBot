7b30c6baef905429a78c98f4240ca8b1b859835b
Akrez <akrez.like@gmail.com>
2024-07-12 16:14:26 +0330

    lang

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'an_authentication_error_occurred' => 'An authentication error occurred',
+    'method_not_allowed' => 'Method not allowed',
+    'url_not_found' => 'URL not found',
+    'resource_not_found' => 'Resource not found',
+    'too_many_request' => 'Too many requests',
+];
---
+++ resources/lang/fa/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'an_authentication_error_occurred' => 'یک خطای احراز هویت رخ داد',
+    'method_not_allowed' => 'متد مجاز نیست',
+    'url_not_found' => 'آدرس یافت نشد',
+    'resource_not_found' => 'منبع یافت نشد',
+    'too_many_request' => 'تعداد درخواست‌ها زیاد است',
+];
