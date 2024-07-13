e676975b469b5158d11df7e8178d1aa1248564c9
Akrez <akrez.like@gmail.com>
2024-07-14 00:09:23 +0330

    trait Enum

--- app/Enums/Enum.php
+++ app/Enums/Enum.php
@@ -2,8 +2,32 @@
 
 namespace App\Enums;
 
+use Illuminate\Support\Facades\Lang;
+
 trait Enum
 {
+    public static function translates()
+    {
+        return [];
+    }
+
+    public static function translatedValues(): array
+    {
+        $translates = self::translates();
+
+        //
+        return collect(self::cases())->pluck('value', 'name')->map(function ($item, $key) use ($translates) {
+            if (isset($translates[$item])) {
+                return $translates[$item];
+            }
+            if (Lang::has($item)) {
+                return __($item);
+            }
+
+            return $key;
+        })->toArray();
+    }
+
     public static function values(): array
     {
         return collect(self::cases())->pluck('value')->toArray();
