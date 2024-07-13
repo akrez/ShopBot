39ec99c9c001a71bfdea83d1bd880fdbcbdd4c8a
Akrez <akrez.like@gmail.com>
2024-07-14 00:49:45 +0330

    blog form

---
+++ resources/views/blogs/_form.blade.php
@@ -0,0 +1,37 @@
+@section('content')
+    <form enctype="multipart/form-data"
+        action="{{ isset($blog) ? route('blogs.update', ['id' => $blog->id]) : route('blogs.store') }}" method="POST">
+        @csrf
+        @if (isset($blog))
+            @method('PUT')
+        @endif
+        @include('components/formBuilder', [
+            'name' => 'name',
+            'value' => isset($blog) ? $blog->name : '',
+        ])
+        @include('components/formBuilder', [
+            'name' => 'short_description',
+            'value' => isset($blog) ? $blog->short_description : '',
+        ])
+        @include('components/formBuilder', [
+            'type' => 'textarea',
+            'name' => 'description',
+            'value' => isset($blog) ? $blog->description : '',
+        ])
+        @include('components/formBuilder', [
+            'name' => 'blog_status',
+            'label' => __('validation.attributes.status'),
+            'type' => 'select',
+            'value' => isset($blog) ? $blog->blog_status : '',
+            'selectOptions' => App\Enums\Blog\BlogStatus::translatedValues(),
+        ])
+        @include('components/formBuilder', [
+            'type' => 'submit',
+            'name' => 'submit',
+            'value' => isset($blog) ? __('Edit') : __('Create'),
+            'size' => 2,
+            'label' => '',
+            'class' => 'btn btn-primary w-100',
+        ])
+    </form>
+@endsection
