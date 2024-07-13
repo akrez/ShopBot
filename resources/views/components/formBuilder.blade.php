c447ac065ec1138bf0deefdb2ecafb268609db82
Akrez <akrez.like@gmail.com>
2024-07-14 00:48:45 +0330

    formbuilder

---
+++ resources/views/components/formBuilder.blade.php
@@ -0,0 +1,91 @@
+@php
+    $inputValue = old($name, isset($value) ? $value : '');
+
+    if (!isset($type)) {
+        $type = 'text';
+    }
+
+    if (!isset($label)) {
+        $label = __('validation.attributes.' . $name);
+    }
+
+    if (!isset($id)) {
+        $id = crc32($label . $name);
+    }
+
+    if (!isset($row)) {
+        $row = true;
+    }
+
+    if (!isset($mt)) {
+        $mt = 2;
+    }
+
+    if (!isset($selectOptions)) {
+        $selectOptions = [];
+    }
+
+    if (!isset($size)) {
+        $size = 4;
+    }
+
+    if (!isset($textareaRows)) {
+        $textareaRows = 4;
+    }
+
+    if (!isset($visible)) {
+        $visible = true;
+    }
+
+    if (!isset($errorsArray)) {
+        $errorsArray = $errors->get($name);
+    }
+
+    if (!isset($class)) {
+        $class = 'form-control';
+    }
+
+    $inputClass = [$class];
+    if ($errorsArray) {
+        $inputClass[] = 'is-invalid';
+    }
+@endphp
+
+@if ($visible)
+    @if ($row)
+        <div class="row">
+    @endif
+    <div class="col-md-{{ $size }} mt-{{ $mt }}">
+        <div class="form-group">
+            @if ($label)
+                <label class="form-label" for="{{ $id }}">{{ $label }}</label>
+            @endif
+            @if ('textarea' == $type)
+                <textarea name="{{ $name }}" id="{{ $id }}" class="{{ implode(' ', $inputClass) }}"
+                    rows="{{ $textareaRows }}">{{ $inputValue }}</textarea>
+            @elseif ('file' == $type)
+                <input name="{{ $name }}" type="{{ $type }}" id="{{ $id }}"
+                    class="{{ implode(' ', $inputClass) }}" value="{{ $inputValue }}" />
+            @elseif ('select' == $type)
+                <select name="{{ $name }}" id="{{ $id }}" class="{{ implode(' ', $inputClass) }}"
+                    value="{{ $inputValue }}">
+                    @foreach ($selectOptions as $selectOptionValue => $selectOption)
+                        <option value="{{ $selectOptionValue }}"
+                            {{ $inputValue == $selectOptionValue ? ' selected="selected" ' : '' }}>
+                            {{ $selectOption }}
+                        </option>
+                    @endforeach
+                </select>
+            @else
+                <input name="{{ $name }}" type="{{ $type }}" id="{{ $id }}"
+                    class="{{ implode(' ', $inputClass) }}" value="{{ $inputValue }}" />
+            @endif
+            @foreach ($errorsArray as $error)
+                <div class="invalid-feedback">{{ $error }}</div>
+            @endforeach
+        </div>
+    </div>
+    @if ($row)
+        </div>
+    @endif
+@endif
