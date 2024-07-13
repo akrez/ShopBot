6e6ecbd30eb6651556a8f2d02be5853eb33e97b2
Akrez <akrez.like@gmail.com>
2024-07-14 01:24:00 +0330

    getUserBlog BlogService

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -7,6 +7,13 @@ use App\Models\User;
 
 class BlogService
 {
+    public function getUserBlog(User $user, int $id)
+    {
+        $blog = $user->blogs()->where('id', $id)->first();
+        abort_unless($user and $blog, 404);
+        return $blog;
+    }
+
     public function setUserActiveBlog(User $user, Blog $blog)
     {
         $user->active_blog = $blog->id;
