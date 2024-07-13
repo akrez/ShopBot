95bd5a511f474dc8bb18db06c9a0f9c2800ea3c5
Akrez <akrez.like@gmail.com>
2024-07-14 01:22:41 +0330

    StoreBlogRequest

---
+++ app/Http/Requests/StoreBlogRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Blog\BlogStatus as BlogBlogStatus;
+use App\Enums\BlogStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreBlogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:64',
+            'short_description' => 'required|max:120',
+            'description' => 'required|max:512',
+            'blog_status' => [Rule::in(BlogBlogStatus::values())],
+        ];
+    }
+}
