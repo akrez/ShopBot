d4687adde4070d239e83aae7542f649a3d706420
Akrez <akrez.like@gmail.com>
2024-07-14 00:46:05 +0330

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Blog\BlogStatus;
+use App\Models\Blog;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'AliAkbar Rezaei',
+            'email' => 'akrez.like@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $blog = Blog::factory()->create([
+            'name' => 'Shahab Tahrir',
+            'short_description' => 'نامی مطمئن',
+            'description' => 'وجود سابقه زیاد و تجربه کافی که گواه آن تولید محصولات با کیفیت و با تنوع بالا در زمینه ملزومات اداری است ما را بر این داشته تا خود را در جهت رضایت هر چه بیشتری مشتریان قرار دهیم',
+            'created_by' => $user,
+            'blog_status' => BlogStatus::ACTIVE,
         ]);
     }
 }
