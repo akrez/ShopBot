a33b34787f37c83738642982142bc02f6c18c880
Akrez <akrez.like@gmail.com>
2024-07-15 03:05:05 +0330

    format

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,43 +3,44 @@
 @section('header', __('Products'))
 
 @section('content')
-<div class="row mb-2">
-    <div class="col-md-2 pull-right">
-        <a class="btn btn-light border border-dark w-100" href="{{ route('products.create') }}">
-            @lang("Create :name", ['name' => __('Product')])
-        </a>
+    <div class="row mb-2">
+        <div class="col-md-2 pull-right">
+            <a class="btn btn-light border border-dark w-100" href="{{ route('products.create') }}">
+                @lang('Create :name', ['name' => __('Product')])
+            </a>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-        <table class="table table-striped table-hover table-bordered align-middle rounded-3">
-            <thead class="table-dark">
-                <tr>
-                    <th scope="col">@lang('validation.attributes.code')</th>
-                    <th scope="col">@lang('validation.attributes.name')</th>
-                    <th scope="col">@lang('validation.attributes.status')</th>
-                    <th scope="col">@lang('validation.attributes.created_at')</th>
-                    <th scope="col">@lang('validation.attributes.updated_at')</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($products as $product)
-                <tr>
-                    <td>{{ $product->code }}</td>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->product_status->trans() }}</td>
-                    <td>{{ $product->created_at }}</td>
-                    <td>{{ $product->updated_at }}</td>
-                    <td>
-                        <a class="btn btn-light border border-dark w-100" href="{{ route('products.edit',['id' => $product->id]) }}">
-                            @lang('Edit')
-                        </a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-striped table-hover table-bordered align-middle rounded-3">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">@lang('validation.attributes.code')</th>
+                        <th scope="col">@lang('validation.attributes.name')</th>
+                        <th scope="col">@lang('validation.attributes.status')</th>
+                        <th scope="col">@lang('validation.attributes.created_at')</th>
+                        <th scope="col">@lang('validation.attributes.updated_at')</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->code }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->product_status->trans() }}</td>
+                            <td>{{ $product->created_at }}</td>
+                            <td>{{ $product->updated_at }}</td>
+                            <td>
+                                <a class="btn btn-light border border-dark w-100"
+                                    href="{{ route('products.edit', ['id' => $product->id]) }}">
+                                    @lang('Edit')
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
