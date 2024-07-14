f51887c33b71b374d8e6e38d13ced67496d03b39
Akrez <akrez.like@gmail.com>
2024-07-15 02:14:51 +0330

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Enums\Blog\BlogStatus;
+use App\Enums\Product\ProductStatus;
 use App\Models\Blog;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,17 +18,24 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $user = User::factory()->create([
-            'name' => 'AliAkbar Rezaei',
+            'name' => 'علی اکبر رضایی',
             'email' => 'akrez.like@gmail.com',
             'password' => bcrypt('12345678'),
         ]);
 
         $blog = Blog::factory()->create([
-            'name' => 'Shahab Tahrir',
+            'name' => 'شهاب تحریر',
             'short_description' => 'نامی مطمئن',
             'description' => 'وجود سابقه زیاد و تجربه کافی که گواه آن تولید محصولات با کیفیت و با تنوع بالا در زمینه ملزومات اداری است ما را بر این داشته تا خود را در جهت رضایت هر چه بیشتری مشتریان قرار دهیم',
             'created_by' => $user,
             'blog_status' => BlogStatus::ACTIVE,
+        ]);
+
+        $product = Product::factory()->create([
+            'code' => 'n36',
+            'name' => 'پایه چسب N36',
+            'blog_id' => $blog->id,
+            'product_status' => ProductStatus::ACTIVE,
         ]);
     }
 }
