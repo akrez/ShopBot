31f9b450f0a6944257ce8a7ea2de1b3ab548a94c
akrez <akrez.like@gmail.com>
2024-07-14 15:16:25 +0330

    web route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SiteController;
 use App\Http\Middleware\ActiveBlogMiddleware;
 use App\Providers\AppServiceProvider;
@@ -14,7 +15,9 @@ Route::get('/', [SiteController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
     Route::get(AppServiceProvider::HOME, [HomeController::class, 'index'])->name('home');
-    Route::resource('blogs', BlogController::class)->parameter('blogs', 'id');
     Route::patch('blogs/{id}/active', [BlogController::class, 'active'])->name('blogs.active');
-    Route::middleware(ActiveBlogMiddleware::class)->group(function () {});
+    Route::resource('blogs', BlogController::class)->parameter('blogs', 'id');
+    Route::middleware(ActiveBlogMiddleware::class)->group(function () {
+        Route::resource('products', ProductController::class)->parameter('products', 'id');
+    });
 });
