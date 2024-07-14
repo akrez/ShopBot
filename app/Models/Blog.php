2beee84c6d948879fd60a30e4bc6a7c66257787f
akrez <akrez.like@gmail.com>
2024-07-14 15:15:08 +0330

    Blog Model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,6 +6,7 @@ use App\Enums\Blog\BlogStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Blog extends Model
 {
@@ -28,6 +29,11 @@ class Blog extends Model
         return [
             'blog_status' => BlogStatus::class,
         ];
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
     }
 
     public function user(): BelongsTo
