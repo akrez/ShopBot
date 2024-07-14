f436cd6f612a35490c3a9d240afedc03fd1ef3d5
akrez <akrez.like@gmail.com>
2024-07-14 11:26:26 +0330

    ActiveBlog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,15 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    @if (app('ActiveBlog')->has())
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ app('ActiveBlog')->name() }}
+                        </a>
+                    </li>
+                    @endif
+                </ul>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     @guest
                     @if (Route::has('login'))
