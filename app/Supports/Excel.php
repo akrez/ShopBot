4baeec5b99950d6702c4183a2df534616a011228
Akrez <akrez.like@gmail.com>
2024-07-15 14:38:03 +0330

    Excel Supports

---
+++ app/Supports/Excel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Supports;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx as XlsxWriter;
+
+class Excel
+{
+    public function export($fileName, $sheetNameSource)
+    {
+        $headers = [
+            'Expires' => '0',
+            'Content-Encoding' => 'UTF-8',
+            'Content-type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ];
+
+        $callback = function () use ($sheetNameSource) {
+            $spreadsheet = new Spreadsheet();
+            //
+            $ctiveSheetIndex = $spreadsheet->getActiveSheetIndex();
+            $spreadsheet->removeSheetByIndex($ctiveSheetIndex);
+            //
+            foreach ($sheetNameSource as $sheetName => $sheetSource) {
+                $sheet = $spreadsheet->createSheet();
+                $sheet->setCodeName($sheetName);
+                $sheet->setTitle($sheetName);
+                $sheet->setRightToLeft(true);
+                $sheet->fromArray($sheetSource);
+                foreach ($sheet->getColumnIterator() as $column) {
+                    $sheet->getColumnDimension($column->getColumnIndex())->setAutoSize(true);
+                }
+            }
+            $writer = new XlsxWriter($spreadsheet);
+            $writer->save('php://output');
+        };
+
+        return response()->streamDownload($callback, $fileName, $headers);
+    }
+}
