bf5b95bdf2d62e1dafeeae24b92061cfc909a5e0
Akrez <akrez.like@gmail.com>
2024-07-17 00:20:31 +0330

    WebResponse

---
+++ app/Supports/WebResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Supports;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class WebResponse implements Responsable
+{
+    public function __construct(
+        private ResponseBuilder $responseBuilder,
+        private ?string $successfulRedirectTo = null
+    ) {}
+
+    public function toResponse($request)
+    {
+        if ($this->responseBuilder->isSuccessful()) {
+            return redirect()
+                ->to($this->successfulRedirectTo)
+                ->with('message', $this->responseBuilder->getMessage());
+        } else {
+            return back()
+                ->with('message', $this->responseBuilder->getMessage())
+                ->withErrors($this->responseBuilder->getErrors())
+                ->withInput($request->input());
+        }
+    }
+}
