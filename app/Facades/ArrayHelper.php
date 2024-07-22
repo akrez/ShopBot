94f6e39dd2f9beca3c4ff82106e546d88311a95c
akrez <akrez.like@gmail.com>
2024-07-22 12:05:29 +0330

    ArrayHelper

--- app/Facades/ArrayHelper.php
+++ app/Facades/ArrayHelper.php
@@ -4,6 +4,12 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static mixed iexplode($delimiters, $string, $limit = PHP_INT_MAX)
+ * @method static mixed filterArray($array, $doFilter = true, $checkUnique = true, $doTrim = true)
+ * @method static mixed templatedArray($template = [], $values = [], $const = [])
+ * @method static mixed trimRecursive($array)
+ */
 class ArrayHelper extends Facade
 {
     /**
