c6d994845794006ca8a1112ece998ba6629ca684
akrez <akrez.like@gmail.com>
2024-07-24 17:57:35 +0330

    up++

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-7 text-center">
             <h1 class="display-4 fw-bold lh-1">تجارت بدون مرز!</h1>
-            <figure class="lead p-3 mb-0 rounded">
+            <figure class="lead p-3 mb-0 rounded-3">
                 <blockquote class="blockquote">
                     <p>
                         اگر کسب و کار شما اینترنتی نیست متاسفانه شما صاحب یک کسب و کار از رده خارج و رو به زوال هستید اگر کسب و کارتان در اینترنت نباشد به زودی از بازار هم حذف خواهید شد
