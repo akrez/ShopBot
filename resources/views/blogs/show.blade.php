502641a1eeb9d7beb4bb3510569fbe97160909a7
Akrez <akrez.like@gmail.com>
2024-07-31 02:43:57 +0330

    up++

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,3 +1,14 @@
+@php
+    $title = \Arr::get($data, 'name');
+    $shortDescription = \Arr::get($data, 'short_description', '');
+    $description = \Arr::get($data, 'description', '');
+    $titleShortDescription = $title . ($shortDescription ? ' | ' . $shortDescription : '');
+    $tags = collect(Arr::get($data, 'products', []))->pluck('product_tags')->flatten()->unique()->toArray();
+    $products = collect(Arr::get($data, 'products', []));
+    $contacts = collect(Arr::get($data, 'contacts', []));
+    $contactSize = max(4, intval(12 / count($contacts)));
+    $logoUrl = \Arr::get($data, 'logo.url', null);
+@endphp
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -8,17 +19,15 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="shortcut icon" href="{{ \Arr::get($data, 'logo.url') }}">
+    <link rel="shortcut icon" href="{{ $logoUrl }}">
 
-    <title>
-        {{ \Arr::get($data, 'name') .
-            (\Arr::get($data, 'short_description') ? ' | ' . \Arr::get($data, 'short_description') : '') }}
-    </title>
+    <title>{{ $titleShortDescription }}</title>
+    <meta name="description" content="{{ $description }}">
 
     <!-- CSS files -->
     <link rel="stylesheet" href="{{ url('libs/bootstrap/dist/css/bootstrap.rtl.min.css') }}" />
     <link rel="stylesheet" href="{{ url('libs/vazirmatn/Vazirmatn-font-face.css') }}" />
-    <link rel="stylesheet" href="{{ url('libs/@fortawesome/fontawesome-free/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('libs/bootstrap-icons/font/bootstrap-icons.min.css') }}">
     <link rel="stylesheet" href="{{ url('css/blog.css') }}">
 
     @yield('POS_HEAD')
@@ -29,12 +38,12 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col-sm-3">
-                @if (\Arr::get($data, 'logo.url'))
+                @if ($logoUrl)
                     <div class="row pb-2">
                         <div class="col-sm-12">
                             <a href="" style="text-align: center;">
-                                <img class="w-100 rounded" alt="{{ \Arr::get($data, 'name') }}"
-                                    src="{{ \Arr::get($data, 'logo.url') }}" style="margin: auto;">
+                                <img class="w-100 rounded" alt="{{ $title }}" src="{{ $logoUrl }}"
+                                    style="margin: auto;">
                             </a>
                         </div>
                     </div>
@@ -42,7 +51,12 @@
                 <div class="row mb-2">
                     <div class="col-sm-12">
                         <div class="btn-group-vertical w-100" role="group">
-                            @foreach (collect(Arr::get($data, 'products', []))->pluck('product_tags')->flatten()->unique()->toArray() as $tagKey => $tag)
+                            <input type="radio" class="btn-check" name="vbtn-radio" id="vbtn-checbox-"
+                                autocomplete="off">
+                            <label class="btn btn-outline-dark" for="vbtn-checbox-">
+                                <h4 class="m-1">همه محصولات {{ $title }}</h4>
+                            </label>
+                            @foreach ($tags as $tagKey => $tag)
                                 <input type="radio" class="btn-check" name="vbtn-radio"
                                     id="vbtn-checbox-{{ $tagKey }}" autocomplete="off">
                                 <label class="btn btn-outline-dark" for="vbtn-checbox-{{ $tagKey }}">
@@ -55,30 +69,53 @@
             </div>
             <div class="col-sm-9 mb-4">
                 <div class="mb-4">
-                    <h1 class="d-inline-block">{{ \Arr::get($data, 'name') }}</h1>
-                    <h2 class="d-inline-block ms-2 text-secondary">{{ \Arr::get($data, 'short_description') }}</h2>
-                    <h4 class="text-justify">{{ \Arr::get($data, 'description') }}</h4>
+                    <h1 class="d-inline-block">{{ $title }}</h1>
+                    <h2 class="d-inline-block ms-2 text-secondary">{{ $shortDescription }}</h2>
+                    <h4 class="text-justify">{{ $description }}</h4>
                 </div>
                 <div class="container-fluid">
                     <div class="row equal">
-                        @foreach (Arr::get($data, 'products', []) as $product1)
-                            @foreach (Arr::get($data, 'products', []) as $product)
-                                <div class="thumbnail border pt-3 pb-3 col-sm-6 col-md-4 col-lg-3">
-                                    @if (isset($product['images'][0]))
-                                        <img class="w-100 pb-2 rounded" src="{{ $product['images'][0]['url'] }}"
-                                            alt="{{ $product['name'] }}">
-                                    @endif
-                                    <div class="card-body">
-                                        <h5 class="card-title font-weight-bold pb-2">{{ $product['name'] }}</h5>
-                                        <p class="card-text">
-                                            @foreach ($product['product_properties'] as $property)
-                                                <strong>{{ $property['property_key'] }}:</strong>
-                                                {{ implode(', ', $property['property_values']) }}<br>
+                        @foreach ($products as $product)
+                            <div class="thumbnail border pt-3 pb-3 col-sm-6 col-md-4 col-lg-3">
+                                @if (count($product['images']) == 1)
+                                    <img class="w-100 pb-3 rounded" src="{{ $product['images'][0]['url'] }}"
+                                        alt="{{ $product['name'] }}">
+                                @elseif (count($product['images']) > 1)
+                                    <div id="product-carousel-{{ $product['code'] }}"
+                                        class="carousel pb-3 carousel-dark slide">
+                                        <div class="carousel-inner">
+                                            @foreach ($product['images'] as $productImage)
+                                                <div
+                                                    class="carousel-item @if ($loop->first) active @endif">
+                                                    <img class="w-100 pb-2 rounded" src="{{ $productImage['url'] }}"
+                                                        alt="{{ $product['name'] }}">
+                                                </div>
                                             @endforeach
-                                        </p>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button"
+                                            data-bs-target="#product-carousel-{{ $product['code'] }}"
+                                            data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button"
+                                            data-bs-target="#product-carousel-{{ $product['code'] }}"
+                                            data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
                                     </div>
+                                @endif
+                                <div class="card-body">
+                                    <h5 class="card-title font-weight-bold pb-2">{{ $product['name'] }}</h5>
+                                    <p class="card-text">
+                                        @foreach ($product['product_properties'] as $property)
+                                            <strong>{{ $property['property_key'] }}:</strong>
+                                            {{ implode(', ', $property['property_values']) }}<br>
+                                        @endforeach
+                                    </p>
                                 </div>
-                            @endforeach
+                            </div>
                         @endforeach
                     </div>
                 </div>
@@ -88,11 +125,34 @@
     <footer class="footer mt-auto py-3 bg-light">
         <div class="container">
             <div class="row">
-                <div class="col-sm-9 pt-2 text-justify">
-                </div>
-                <div class="col-sm-3" dir="ltr">
-                    <h6 class="text-uppercase fw-bold mb-4">ارتباط با ما</h6>
-                </div>
+                @foreach ($contacts as $contact)
+                    @php
+                        if ('address' == $contact['contact_type']) {
+                            $icon = 'bi bi-geo-alt';
+                        } elseif ('telegram' == $contact['contact_type']) {
+                            $icon = 'bi bi-telegram';
+                        } elseif ('whatsapp' == $contact['contact_type']) {
+                            $icon = 'bi bi-whatsapp';
+                        } elseif ('email' == $contact['contact_type']) {
+                            $icon = 'bi bi-envelope';
+                        } elseif ('instagram' == $contact['contact_type']) {
+                            $icon = 'bi bi-instagram';
+                        } else {
+                            $icon = 'bi bi-telephone';
+                        }
+                    @endphp
+                    <div class="col-lg-{{ $contactSize }} pt-3">
+                        <div class="info-item text-center">
+                            <div class="contact d-inline-block text-center">
+                                <div class="d-flex justify-content-center"><i class="{{ $icon }} fs-3em"></i>
+                                </div>
+                                <h3>{{ $contact['contact_key'] }}</h3>
+                                <p><a href="{{ $contact['contact_link'] }}"
+                                        dir="ltr">{{ $contact['contact_value'] }}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </footer>
