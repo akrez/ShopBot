b09d313c5ce223cdd52f05f9690944a68cfdbdd9
Akrez <akrez.like@gmail.com>
2024-08-23 20:26:20 +0330

    fix

--- resources/views/contacts/_form.blade.php
+++ resources/views/contacts/_form.blade.php
@@ -1,38 +1,38 @@
-@section('content')
-<form enctype="multipart/form-data" action="{{ isset($contact) ? route('contacts.update', ['id' => $contact->id]) : route('contacts.store') }}" method="POST">
+<form enctype="multipart/form-data"
+    action="{{ isset($contact) ? route('contacts.update', ['id' => $contact->id]) : route('contacts.store') }}"
+    method="POST">
     @csrf
     @if (isset($contact))
-    @method('PUT')
+        @method('PUT')
     @endif
     @include('components/formBuilder', [
-    'name' => 'contact_type',
-    'type' => 'select',
-    'value' => isset($contact) ? $contact->contact_type->value : '',
-    'selectOptions' => ['' => ''] + \App\Enums\Contact\ContactType::toArray(),
+        'name' => 'contact_type',
+        'type' => 'select',
+        'value' => isset($contact) ? $contact->contact_type->value : '',
+        'selectOptions' => ['' => ''] + \App\Enums\Contact\ContactType::toArray(),
     ])
     @include('components/formBuilder', [
-    'name' => 'contact_key',
-    'value' => isset($contact) ? $contact->contact_key : '',
+        'name' => 'contact_key',
+        'value' => isset($contact) ? $contact->contact_key : '',
     ])
     @include('components/formBuilder', [
-    'name' => 'contact_value',
-    'value' => isset($contact) ? $contact->contact_value : '',
+        'name' => 'contact_value',
+        'value' => isset($contact) ? $contact->contact_value : '',
     ])
     @include('components/formBuilder', [
-    'name' => 'contact_link',
-    'value' => isset($contact) ? $contact->contact_link : '',
+        'name' => 'contact_link',
+        'value' => isset($contact) ? $contact->contact_link : '',
     ])
     @include('components/formBuilder', [
-    'name' => 'contact_order',
-    'value' => isset($contact) ? $contact->contact_order : '',
+        'name' => 'contact_order',
+        'value' => isset($contact) ? $contact->contact_order : '',
     ])
     @include('components/formBuilder', [
-    'type' => 'submit',
-    'name' => 'submit',
-    'value' => isset($contact) ? __('Edit') : __('Create'),
-    'size' => 2,
-    'label' => '',
-    'class' => 'btn w-100 ' . (isset($contact) ? 'btn-primary' : 'btn-success'),
+        'type' => 'submit',
+        'name' => 'submit',
+        'value' => isset($contact) ? __('Edit') : __('Create'),
+        'size' => 2,
+        'label' => '',
+        'class' => 'btn w-100 ' . (isset($contact) ? 'btn-primary' : 'btn-success'),
     ])
 </form>
-@endsection
--- resources/views/products/_form.blade.php
+++ resources/views/products/_form.blade.php
@@ -1,40 +1,38 @@
-@section('content')
-    <form enctype="multipart/form-data"
-        action="{{ isset($product) ? route('products.update', ['id' => $product->id]) : route('products.store') }}"
-        method="POST">
-        @csrf
-        @if (isset($product))
-            @method('PUT')
-        @endif
-        @include('components/formBuilder', [
-            'name' => 'code',
-            'value' => isset($product) ? $product->code : '',
-            'hints' => [__('validation.code', ['Attribute' => __('validation.attributes.code')])],
-        ])
-        @include('components/formBuilder', [
-            'name' => 'name',
-            'value' => isset($product) ? $product->name : '',
-        ])
-        @include('components/formBuilder', [
-            'name' => 'product_status',
-            'label' => __('validation.attributes.status'),
-            'type' => 'select',
-            'value' => isset($product) ? $product->product_status->value : '',
-            'selectOptions' => App\Enums\Product\ProductStatus::toArray(),
-        ])
-        @include('components/formBuilder', [
-            'name' => 'product_order',
-            'label' => __('validation.attributes.product_order'),
-            'errorsArray' => $errors->get('product_order'),
-            'value' => isset($product) ? $product->product_order : '',
-        ])
-        @include('components/formBuilder', [
-            'type' => 'submit',
-            'name' => 'submit',
-            'value' => isset($product) ? __('Edit') : __('Create'),
-            'size' => 2,
-            'label' => '',
-            'class' => 'btn w-100 ' . (isset($product) ? 'btn-primary' : 'btn-success'),
-        ])
-    </form>
-@endsection
+<form enctype="multipart/form-data"
+    action="{{ isset($product) ? route('products.update', ['id' => $product->id]) : route('products.store') }}"
+    method="POST">
+    @csrf
+    @if (isset($product))
+        @method('PUT')
+    @endif
+    @include('components/formBuilder', [
+        'name' => 'code',
+        'value' => isset($product) ? $product->code : '',
+        'hints' => [__('validation.code', ['Attribute' => __('validation.attributes.code')])],
+    ])
+    @include('components/formBuilder', [
+        'name' => 'name',
+        'value' => isset($product) ? $product->name : '',
+    ])
+    @include('components/formBuilder', [
+        'name' => 'product_status',
+        'label' => __('validation.attributes.status'),
+        'type' => 'select',
+        'value' => isset($product) ? $product->product_status->value : '',
+        'selectOptions' => App\Enums\Product\ProductStatus::toArray(),
+    ])
+    @include('components/formBuilder', [
+        'name' => 'product_order',
+        'label' => __('validation.attributes.product_order'),
+        'errorsArray' => $errors->get('product_order'),
+        'value' => isset($product) ? $product->product_order : '',
+    ])
+    @include('components/formBuilder', [
+        'type' => 'submit',
+        'name' => 'submit',
+        'value' => isset($product) ? __('Edit') : __('Create'),
+        'size' => 2,
+        'label' => '',
+        'class' => 'btn w-100 ' . (isset($product) ? 'btn-primary' : 'btn-success'),
+    ])
+</form>
